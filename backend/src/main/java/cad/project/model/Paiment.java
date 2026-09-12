9db422037aad263c0e30e77f070f10a05c34a65b
YOUSSEF SABBAB <sabbabyoussef@gmail.com>
2026-09-12 12:37:31 +0100

    fix ci pipeline: add postgres service + fix validation bug

--- backend/src/main/java/cad/project/model/Paiment.java
+++ backend/src/main/java/cad/project/model/Paiment.java
@@ -2,6 +2,7 @@ package cad.project.model;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
@@ -29,7 +30,7 @@ public class Paiment {
     @JoinColumn(name = "client_id")
     private  Client client;
 
-    @NotBlank
+    @NotNull
     private double  montant_Paye;
 
     private  String method;
